edcd952d6bf0b45e0d3773ef66e234c1f7353302
she110ff <youngsoo.j@gmail.com>
2023-10-31 11:15:24 +0900

    Add mobx state

--- app/about.js
+++ app/about.js
@@ -1,12 +1,23 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View} from "react-native";
 import { Link } from "expo-router";
+import { observer } from "mobx-react"
+import {useStores} from "../stores";
 
-export default function About() {
+const About = observer( () => {
+    const { noteStore, userStore } = useStores();
     return (
+        <View>
         <Link href="/" asChild>
             <Pressable>
                 <Text>Home</Text>
             </Pressable>
         </Link>
+
+            <Pressable onPress={() => userStore.setUserName('Terry')}>
+                <Text>Home {userStore.name}</Text>
+            </Pressable>
+        </View>
     );
-}
+})
+
+export default About;
---
+++ stores/note.store.js
@@ -0,0 +1,12 @@
+import { makeAutoObservable } from "mobx";
+
+class NoteStore {
+    notes = [];
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+}
+
+export default NoteStore;
