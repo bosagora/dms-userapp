d6a3fc3dfb0955d481725e440b12674c27ff1352
she110ff <youngsoo.j@gmail.com>
2023-10-31 19:32:30 +0900

    Add Stack and Tab navigator

--- screens/Test.js
+++ screens/Test.js
@@ -1,15 +1,15 @@
 import {Button, View, Text} from "react-native";
 
 
-export default function DetailsScreen({ navigation }) {
+export default function Test({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
+            <Text>Test Screen</Text>
             <Button
-                title="Go to Test"
-                onPress={() => navigation.navigate('Test')}
+                title="Go to Detail"
+                onPress={() => navigation.navigate('Detail')}
             />
-            <Button title="Go to Home" onPress={() => navigation.navigate('Wallet')} />
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
             <Button title="Go back" onPress={() => navigation.goBack()} />
         </View>
     );
