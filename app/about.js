98890c40c2a6a670a4d0abccab53480aa58069ae
she110ff <youngsoo.j@gmail.com>
2023-10-31 19:56:54 +0900

    Add prettier

--- app/about.js
+++ app/about.js
@@ -1,23 +1,23 @@
-import { Pressable, Text, View} from "react-native";
-import { Link } from "expo-router";
-import { observer } from "mobx-react"
-import {useStores} from "../stores";
+import { Pressable, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { observer } from 'mobx-react';
+import { useStores } from '../stores';
 
-const About = observer( () => {
-    const { noteStore, userStore } = useStores();
-    return (
-        <View>
-        <Link href="/" asChild>
-            <Pressable>
-                <Text>Home</Text>
-            </Pressable>
-        </Link>
+const About = observer(() => {
+ const { noteStore, userStore } = useStores();
+ return (
+  <View>
+   <Link href='/' asChild>
+    <Pressable>
+     <Text>Home</Text>
+    </Pressable>
+   </Link>
 
-            <Pressable onPress={() => userStore.setUserName('sony')}>
-                <Text>Home {userStore.name}</Text>
-            </Pressable>
-        </View>
-    );
-})
+   <Pressable onPress={() => userStore.setUserName('sony')}>
+    <Text>Home {userStore.name}</Text>
+   </Pressable>
+  </View>
+ );
+});
 
 export default About;
