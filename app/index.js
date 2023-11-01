d4248e0b56dbc9503465f260ca2030f02c309716
she110ff <youngsoo.j@gmail.com>
2023-11-01 16:43:43 +0900

    Add bottom sheet

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import {
-	ActivityIndicator,
-	Button,
-	StyleSheet,
-	Text,
-	View,
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -19,134 +19,142 @@ import Wallet from '../screens/Wallet';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Test from '../screens/Test';
 import About from '../screens/About';
+import ActionSheetScreen from '../screens/ActionSheet';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-	const [isStoreLoaded, setIsStoreLoaded] = useState(false);
+  const [isStoreLoaded, setIsStoreLoaded] = useState(false);
 
-	useEffect(() => {
-		const rehydrate = async () => {
-			await trunk.init();
-			setIsStoreLoaded(true);
-		};
-		rehydrate();
-	}, []);
+  useEffect(() => {
+    const rehydrate = async () => {
+      await trunk.init();
+      setIsStoreLoaded(true);
+    };
+    rehydrate();
+  }, []);
 
-	function SearchScreen() {
-		return <Text>Search</Text>;
-	}
+  function SearchScreen() {
+    return <Text>Search</Text>;
+  }
 
-	function NotificationScreen() {
-		return <Text>Notification</Text>;
-	}
+  function NotificationScreen() {
+    return <Text>Notification</Text>;
+  }
 
-	function MessageScreen({ navigation }) {
-		return (
-			<View style={styles.main}>
-				<Button
-					title='Go to Details... again'
-					onPress={() => navigation.navigate('Detail')}
-				/>
-			</View>
-		);
-	}
-	function MyTab() {
-		return (
-			<Tab.Navigator
-				initialRouteName='Wallet'
-				screenOptions={{ headerShown: false }}>
-				<Tab.Screen
-					name='Wallet'
-					component={Wallet}
-					options={{
-						title: '홈',
-						tabBarIcon: ({ color, size }) => (
-							<Icon name='home' color={color} size={size} />
-						),
-					}}
-				/>
-				<Tab.Screen
-					name='Search'
-					component={SearchScreen}
-					options={{
-						title: '검색',
-						tabBarIcon: ({ color, size }) => (
-							<Icon name='search' color={color} size={size} />
-						),
-					}}
-				/>
-				<Tab.Screen
-					name='Notification'
-					component={NotificationScreen}
-					options={{
-						title: '알림',
-						tabBarIcon: ({ color, size }) => (
-							<Icon name='notifications' color={color} size={size} />
-						),
-					}}
-				/>
-				<Tab.Screen
-					name='Message'
-					component={MessageScreen}
-					options={{
-						title: '메시지',
-						tabBarIcon: ({ color, size }) => (
-							<Icon name='message' color={color} size={size} />
-						),
-					}}
-				/>
-			</Tab.Navigator>
-		);
-	}
+  function MessageScreen({ navigation }) {
+    return (
+      <View style={styles.main}>
+        <Button
+          title='Go to Details... again'
+          onPress={() => navigation.navigate('Detail')}
+        />
+      </View>
+    );
+  }
+  function MyTab() {
+    return (
+      <Tab.Navigator
+        initialRouteName='Wallet'
+        screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name='Wallet'
+          component={Wallet}
+          options={{
+            title: '홈',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Search'
+          component={SearchScreen}
+          options={{
+            title: '검색',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='search' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Notification'
+          component={NotificationScreen}
+          options={{
+            title: '알림',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='notifications' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Message'
+          component={MessageScreen}
+          options={{
+            title: '메시지',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='message' color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
 
-	const { noteStore, userStore } = useStores();
+  const { noteStore, userStore } = useStores();
 
-	if (!isStoreLoaded) {
-		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<ActivityIndicator size='large' />
-			</View>
-		);
-	} else {
-		return (
-			<NavigationContainer independent={true}>
-				<GluestackUIProvider config={config}>
-					<Stack.Navigator>
-						<Stack.Screen
-							name='MyTab'
-							component={MyTab}
-							options={{ headerShown: false }}
-						/>
-						<Stack.Screen name='Detail' component={DetailsScreen} />
-						<Stack.Screen name='About' component={About} />
-						<Stack.Screen name='Test' component={Test} />
-					</Stack.Navigator>
-				</GluestackUIProvider>
-			</NavigationContainer>
-		);
-	}
+  if (!isStoreLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  } else {
+    return (
+      <BottomSheetModalProvider>
+        <NavigationContainer independent={true}>
+          <GluestackUIProvider config={config}>
+            <Stack.Navigator>
+              <Stack.Screen
+                name='MyTab'
+                component={MyTab}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name='Detail' component={DetailsScreen} />
+              <Stack.Screen
+                name='ActionSheetScreen'
+                component={ActionSheetScreen}
+              />
+              <Stack.Screen name='About' component={About} />
+              <Stack.Screen name='Test' component={Test} />
+            </Stack.Navigator>
+          </GluestackUIProvider>
+        </NavigationContainer>
+      </BottomSheetModalProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		padding: 24,
-	},
-	main: {
-		flex: 1,
-		justifyContent: 'center',
-		maxWidth: 960,
-		marginHorizontal: 'auto',
-	},
-	title: {
-		fontSize: 64,
-		fontWeight: 'bold',
-	},
-	subtitle: {
-		fontSize: 36,
-		color: '#38434D',
-	},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    maxWidth: 960,
+    marginHorizontal: 'auto',
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 36,
+    color: '#38434D',
+  },
 });
--- screens/Detail.js
+++ screens/Detail.js
@@ -4,15 +4,26 @@ import { observer } from 'mobx-react';
 import React from 'react';
 
 const DetailsScreen = observer(({ navigation }) => {
-	const { noteStore, userStore } = useStores();
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Details Screen user : {userStore.name}</Text>
-			<Button title='Go to About' onPress={() => navigation.navigate('About')} />
-			<Button title='Go to Home' onPress={() => navigation.navigate('Wallet')} />
-			<Button title='Go back' onPress={() => navigation.goBack()} />
-		</View>
-	);
+  const { noteStore, userStore } = useStores();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen user : {userStore.name}</Text>
+      <Button title='Go to Test' onPress={() => navigation.navigate('Test')} />
+      <Button
+        title='Go to About'
+        onPress={() => navigation.navigate('About')}
+      />
+      <Button
+        title='Go to ActionSheetScreen'
+        onPress={() => navigation.navigate('ActionSheetScreen')}
+      />
+      <Button
+        title='Go to Home'
+        onPress={() => navigation.navigate('Wallet')}
+      />
+      <Button title='Go back' onPress={() => navigation.goBack()} />
+    </View>
+  );
 });
 
 export default DetailsScreen;
