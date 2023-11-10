3c2887c944fa285f5f0ca626f460699023f771ab
she110ff <youngsoo.j@gmail.com>
2023-11-10 13:35:15 +0900

    Add ether wallet creation

--- screens/initScreens/Secret.js
+++ screens/initScreens/Secret.js
@@ -3,19 +3,32 @@ import { trunk, useStores } from '../../stores';
 import { observer } from 'mobx-react';
 import React, { useEffect } from 'react';
 import { PinCodeT } from '@anhnch/react-native-pincode';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import { ethers } from 'ethers'; //for ethers.js
+const { Wallet } = ethers;
 
 const Secret = observer(({ navigation }) => {
-  const { pinStore, userStore } = useStores();
+    const { pinStore, userStore } = useStores();
 
-  function resetPinCode() {
-    navigation.navigate('InitPinCodeScreen');
-  }
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Secret Screen</Text>
-      <Button title='Next' onPress={() => resetPinCode()} />
-    </View>
-  );
+    function createWallet() {
+        const wallet = Wallet.createRandom();
+
+        console.log('address :', wallet.address);
+        console.log('mnemonic :', wallet.mnemonic);
+        console.log('privateKey :', wallet.privateKey);
+    }
+    function resetPinCode() {
+        navigation.navigate('InitPinCodeScreen');
+    }
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Secret Screen</Text>
+            <Button title='Create Wallet' onPress={() => createWallet()} />
+            <Button title='Next' onPress={() => resetPinCode()} />
+        </View>
+    );
 });
 
 export default Secret;
