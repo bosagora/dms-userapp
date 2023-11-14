231b7728b7334b6351f70e0affb07abac9e1a1e2
she110ff <youngsoo.j@gmail.com>
2023-11-14 15:50:09 +0900

    Fix Creating Wallet Screen

--- screens/initScreens/Secret.js
+++ screens/initScreens/Secret.js
@@ -1,4 +1,4 @@
-import { Button, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { trunk, useStores } from '../../stores';
 import { observer } from 'mobx-react';
 import React, { useEffect } from 'react';
@@ -6,7 +6,10 @@ import { PinCodeT } from '@anhnch/react-native-pincode';
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
 import { ethers } from 'ethers';
-import { getSecureValue, saveSecureValue } from '../../utils/secure.store'; //for ethers.js
+import { getSecureValue, saveSecureValue } from '../../utils/secure.store';
+import ImportPrivateKey from '../../components/ImportPrivateKey';
+import { Box, ButtonText, Button, Center, VStack } from '@gluestack-ui/themed';
+import MobileHeader from '../../components/MobileHeader'; //for ethers.js
 const { Wallet } = ethers;
 
 const Secret = observer(({ navigation }) => {
@@ -23,17 +26,48 @@ const Secret = observer(({ navigation }) => {
     await saveSecureValue('address', wallet.address);
     await saveSecureValue('mnemonic', JSON.stringify(wallet.mnemonic));
     await saveSecureValue('privateKey', wallet.privateKey);
+    resetPinCode();
   }
   function resetPinCode() {
     navigation.navigate('InitPinCodeScreen');
   }
 
+  async function saveKey(key) {
+    const privateKey = key.includes('0x') ? key.split('0x')[0] : key;
+    console.log('save privateKey :', privateKey);
+    const wallet = new Wallet(key);
+    secretStore.setAddress(wallet.address);
+    await saveSecureValue('address', wallet.address);
+    resetPinCode();
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Secret Screen</Text>
-      <Button title='Create Wallet' onPress={() => createWallet()} />
-      <Button title='Next' onPress={() => resetPinCode()} />
-    </View>
+    <SafeAreaView>
+      <Box
+        sx={{
+          _dark: { bg: '$backgroundDark800' },
+          _web: {
+            height: '100vh',
+            w: '100vw',
+            overflow: 'hidden',
+          },
+        }}
+        height='$full'
+        bg='$backgroundLight0'>
+        <MobileHeader
+          title='지갑 생성'
+          subTitle='마일리지 적립/사용을 위한 지갑'
+        />
+        <VStack space='lg' pt='$4' m='$7'>
+          <Box>
+            <Button py='$2.5' px='$3' onPress={() => createWallet()}>
+              <ButtonText>지갑 생성하기</ButtonText>
+            </Button>
+          </Box>
+          <ImportPrivateKey saveKey={saveKey} />
+        </VStack>
+      </Box>
+    </SafeAreaView>
   );
 });
 
