01f0455d23404fe6374768c8f8e843d8781e3127
she110ff <youngsoo.j@gmail.com>
2023-12-05 13:10:35 +0900

    Add language and currency with storage

--- utils/convert.js
+++ utils/convert.js
@@ -11,5 +11,11 @@ function toFix(str, dec = 2) {
 }
 
 export function convertProperValue(str, dec = 2, trunc = 10) {
-  return truncateString(toFix(str, dec), trunc);
+  return numberWithCommas(truncateString(toFix(str, dec), trunc));
+}
+
+export function numberWithCommas(x) {
+  let parts = x.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
 }
